6ba3b361470465cac730650212e25f359aac606b
69rem <maksfeed54@gmail.com>
2025-12-25 20:33:38 +0300

    WIP: initial implementation, pending refinements

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+const Button = (props) => {
+  const { className = "", type = "button", children, onClick, isDisabled } = props;
+
+  return (
+    <button className={`button ${className}`} type={type} disabled={isDisabled} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
