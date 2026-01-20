9d299aecb524fc713beb568905506794bdb9c757
69rem <maksfeed54@gmail.com>
2026-01-20 12:24:36 +0300

    add animations for adding and deleting tasks

--- src/context/TasksContext.jsx
+++ src/context/TasksContext.jsx
@@ -1,36 +1,15 @@
-import { createContext } from "react";
-import useTasks from "../hooks/useTasks";
-import useIncompleteTaskScroll from "../hooks/useIncompleteTaskScroll";
+import { createContext } from 'react'
+import useTasks from '../hooks/useTasks'
+import useIncompleteTaskScroll from '../hooks/useIncompleteTaskScroll'
 
-export const TasksContext = createContext({});
+export const TasksContext = createContext({})
 
 export const TasksProvider = (props) => {
-  const { children } = props;
+    const { children } = props
 
-  const {
-    tasks,
-    filteredTasks,
-    deleteTask,
-    deleteAllTasks,
-    toggleTaskComplete,
-    newTaskTitle,
-    setNewTaskTitle,
-    searchQuery,
-    setSearchQuery,
-    newTaskInputRef,
-    addTask,
-  } = useTasks();
-
-  const { firstIncompleteTaskRef, firstIncompleteTaskId } =
-    useIncompleteTaskScroll(tasks);
-
-  return (
-    <TasksContext.Provider
-      value={{
+    const {
         tasks,
         filteredTasks,
-        firstIncompleteTaskRef,
-        firstIncompleteTaskId,
         deleteTask,
         deleteAllTasks,
         toggleTaskComplete,
@@ -40,9 +19,36 @@ export const TasksProvider = (props) => {
         setSearchQuery,
         newTaskInputRef,
         addTask,
-      }}
-    >
-      {children}
-    </TasksContext.Provider>
-  );
-};
+        disappearingTaskId,
+        appearingTaskId,
+    } = useTasks()
+
+    const {
+        firstIncompleteTaskRef,
+        firstIncompleteTaskId,
+    } = useIncompleteTaskScroll(tasks)
+
+    return (
+        <TasksContext.Provider
+            value={{
+                tasks,
+                filteredTasks,
+                firstIncompleteTaskRef,
+                firstIncompleteTaskId,
+                deleteTask,
+                deleteAllTasks,
+                toggleTaskComplete,
+                newTaskTitle,
+                setNewTaskTitle,
+                searchQuery,
+                setSearchQuery,
+                newTaskInputRef,
+                addTask,
+                disappearingTaskId,
+                appearingTaskId,
+            }}
+        >
+            {children}
+        </TasksContext.Provider>
+    )
+}
